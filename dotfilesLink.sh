a522febd672a5d7b31f4ff5b70613fb540f8f8ab
ytac8 <ytac8@ytac8.local>
2016-12-18 07:47:08 +0900

    add bin link

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,9 @@ ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/plugins.toml ~/.config/dein/plugins.toml
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/bin/battery /usr/local/bin
+ln -sf ~/dotfiles/bin/wifi /usr/local/bin
+ln -sf ~/dotfiles/bin/get_ssid /usr/local/bin
+
+
