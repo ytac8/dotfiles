984fd633b536bcf31b99bb29a7355dc32b9f0928
ytac8 <y.tac0223@gmail.com>
2023-04-05 14:24:18 +0900

    add some file

---
+++ setup.sh
@@ -0,0 +1,24 @@
+# Set symbolic link
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# brew install
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install btop
+brew install cmake
+brew install coreutils
+brew install curl
+brew install fzf
+brew install fzy
+brew install ghq
+brew install git
+brew install jesseduffield/lazygit/lazygit
+brew install koekeishiya/formulae/skhd
+brew install koekeishiya/formulae/yabai
+brew install neovim
+brew install peco
+brew install pyenv
+brew install rcm
+brew install ripgrep
+brew install tig
+brew install zsh
