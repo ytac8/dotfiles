ba6f0398995e54993d8d74bcdeda4a8b558b8a16
ytac8 <y.tac0223@gmail.com>
2023-04-05 15:00:24 +0900

    fix setup shell

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 # Set symbolic link
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/ghq/github.com/ytac8/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/ghq/github.com/ytac8/dotfiles/.skhdrc ~/.skhdrc
+ln -sf ~/ghq/github.com/ytac8/dotfiles/.yabairc ~/.yabairc
+ln -sf ~/ghq/github.com/ytac8/dotfiles/config/nvim ~/.config/
+ln -sf ~/ghq/github.com/ytac8/dotfiles/config/kitty ~/.config/
+ln -sf ~/ghq/github.com/ytac8/dotfiles/config/peco ~/.config/
+ln -sf ~/ghq/github.com/ytac8/dotfiles/config/pycodestyle ~/.config/pycodestyle
 
 # brew install
 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
