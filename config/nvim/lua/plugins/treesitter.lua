7c60b7498d17585497c54ab757973886408eaa84
ytac8 <y.tac0223@gmail.com>
2026-02-02 01:55:41 +0900

    fix

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,21 +2,8 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	lazy = false,
-	main = "nvim-treesitter.config",
+	main = "nvim-treesitter",
 	opts = {
 		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "bash", "python", "typescript", "go" },
-		sync_install = false,
-		auto_install = true,
-		highlight = {
-			enable = true,
-			disable = function(lang, buf)
-				local max_filesize = 300 * 1024
-				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				if ok and stats and stats.size > max_filesize then
-					return true
-				end
-			end,
-			additional_vim_regex_highlighting = false,
-		},
 	},
 }
